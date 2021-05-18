19d760985827198809de517be3929c17ea7fb37a
Flevertyapp <Flevertyap@gmail.com>
2021-05-18 11:06:26 +0300

    Обязательная часть сделана. Найденные косяки: - не отображается title в описании фото дня, данные приходят, может в xml накосячил(хардкод текст тоже не показывает) - не скроллится и не прячется BottomAppBar - вынести размеры в димен

---
+++ app/src/main/java/ru/example/nasa_pod/ui/main/PictureOfTheDayFragment.kt
@@ -0,0 +1,146 @@
+package ru.example.nasa_pod.ui.main
+
+import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import ru.example.nasa_pod.R
+import ru.example.nasa_pod.ui.MainActivity
+import kotlinx.android.synthetic.main.main_fragment.*
+import coil.api.load
+import android.content.Intent
+import android.net.Uri
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet_layout.*
+import kotlinx.android.synthetic.main.bottom_sheet_layout.view.*
+
+
+class PictureOfTheDayFragment : Fragment() {
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private val viewModel: PictureOfTheDayViewModel by lazy {
+        ViewModelProviders.of(this).get(PictureOfTheDayViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        //поиск в вики
+        input_layout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
+            })
+        }
+        viewModel.getData()
+            .observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+        setBottomAppBar(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(
+            R.menu.menu_bottom_bar,
+            menu
+        ) //если не надуть сначала, не показывает.Ну пачимуууу?
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_settings -> Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+            android.R.id.home -> {
+                activity?.let {
+                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, "tag")
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+        fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                bottom_app_bar.navigationIcon = null
+                bottom_app_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                fab.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_back_fab))
+                bottom_app_bar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                bottom_app_bar.navigationIcon =
+                    ContextCompat.getDrawable(context, R.drawable.ic_hamburger_menu_bottom_bar)
+                bottom_app_bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                fab.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_plus_fab))
+                bottom_app_bar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
+    }
+
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    //тост ошибка
+                    toast("Link is empty")
+                } else {
+                    //тянем картинку
+                    image_view.load(url) {
+                        lifecycle(this@PictureOfTheDayFragment)
+                        error(R.drawable.ic_no_photo_vector)
+                        //placeholder(R.drawable.no_photo)
+                    }
+                    //тянем описание. Так норм вытягивать???
+                    val title = serverResponseData.title
+                    val explanation = serverResponseData.explanation
+                    if (!title.isNullOrEmpty()) {
+                        bottom_sheet_container.bottom_sheet_description.text =
+                            title //не показывает, может в разметке косяк? данные сюда приходят
+                    }
+                    if (!explanation.isNullOrEmpty()) {
+                        bottom_sheet_container.bottom_sheet_description.text = explanation
+                    }
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                image_view.load(R.drawable.loading)
+            }
+            is PictureOfTheDayData.Error -> {
+                toast(data.error.message)
+                image_view.load(R.drawable.ic_no_photo_vector)
+            }
+        }
+    }
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
+    private fun Fragment.toast(string: String?) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.BOTTOM, 0, 250)
+            show()
+        }
+    }
+
+    companion object {
+        fun newInstance() = PictureOfTheDayFragment()
+        private var isMain = true
+    }
+}
